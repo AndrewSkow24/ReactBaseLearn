28a6aff41663dd5706a48a3b0c073aae97c4cc6b
Andrew Skow <anjeie24@mail.ru>
2025-06-25 11:15:51 +0300

    08of112

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
-import Component07 from "./components/Component07";
 
 function App() {
   return (
-    <>
-      <h1>Учебник React</h1>
-    </>
+    <ul>
+      <li>Элемент 1</li>
+      <li>Элемент 2</li>
+      <li>Элемент 3</li>
+      <li>Элемент 4</li>
+      <li>Элемент 5</li>
+      <li>Элемент 6</li>
+      <li>Элемент 7</li>
+      <li>Элемент 8</li>
+      <li>Элемент 9</li>
+      <li>Элемент 10</li>
+    </ul>
   );
 }
 
