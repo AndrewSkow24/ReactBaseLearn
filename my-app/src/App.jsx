06f5f495c681d8203bd4dbcb736985b45ea6b375
Andrew Skow <anjeie24@mail.ru>
2025-06-25 16:35:04 +0300

    10of112

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 
 function App() {
   return (
-    <ul>
-      <li>text1</li>
-      <li>text2</li>
-      <li>text3</li>
-    </ul>
+    <div>
+      <ul>
+        <li>text1</li>
+        <li>text2</li>
+        <li>text3</li>
+      </ul>
+      <ul>
+        <li>text1</li>
+        <li>text2</li>
+        <li>text3</li>
+      </ul>
+    </div>
   );
 }
 
