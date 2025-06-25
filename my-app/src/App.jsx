3cf8f44eb398c011f6d4add21e9d8deac8f088c4
Andrew Skow <anjeie24@mail.ru>
2025-06-25 16:05:17 +0300

    09of12

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,16 +3,9 @@ import React, { Component } from "react";
 function App() {
   return (
     <ul>
-      <li>Элемент 1</li>
-      <li>Элемент 2</li>
-      <li>Элемент 3</li>
-      <li>Элемент 4</li>
-      <li>Элемент 5</li>
-      <li>Элемент 6</li>
-      <li>Элемент 7</li>
-      <li>Элемент 8</li>
-      <li>Элемент 9</li>
-      <li>Элемент 10</li>
+      <li>text1</li>
+      <li>text2</li>
+      <li>text3</li>
     </ul>
   );
 }
