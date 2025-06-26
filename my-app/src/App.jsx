206c04f10e947d8c32cdcb7ac4d78c738c79c365
Andrew Skow <anjeie24@mail.ru>
2025-06-27 00:02:50 +0300

    31of112

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 
-const App = () => {
-  const isAuth = false;
+function App() {
+  function getDigitsSum(num) {
+    let numInString = String(num);
+    let sumOfNum = 0;
+    for (let i = 0; i < numInString.length; i++) {
+      console.log(numInString[i]);
+      sumOfNum += Number(numInString[i]);
+    }
 
-  return <div>{!isAuth && <p>Пожалуйста, авторизируйтесь!</p>}</div>;
-};
+    return sumOfNum;
+  }
+
+  return (
+    <div>
+      <p>Сумма цифр числа 123 составляет: {getDigitsSum(123)} </p>
+      <p>Сумма цифр числа 57 составляет: {getDigitsSum(57)} </p>
+    </div>
+  );
+}
 
 export default App;
