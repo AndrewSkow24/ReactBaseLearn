f48ed889884f9d76010df6cdf449b95133959bf5
Andrew Skow <anjeie24@mail.ru>
2025-06-26 23:38:38 +0300

    30of112

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,19 +1,9 @@
 import React from "react";
 
-function App() {
-  const isAdult = false;
+const App = () => {
+  const isAuth = false;
 
-  let textForUser = "";
-  if (isAdult)
-    textForUser = <p>Но ты уже взрослый, и больше не будешь слушать сказки</p>;
-  else textForUser = <p>Возможно у тебя есть паспорт, но тебе нет 18</p>;
-
-  return (
-    <div>
-      <h1>Тест совершеннолетия:</h1>
-      {textForUser}
-    </div>
-  );
-}
+  return <div>{!isAuth && <p>Пожалуйста, авторизируйтесь!</p>}</div>;
+};
 
 export default App;
